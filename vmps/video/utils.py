b804f6db2a3049abd52f00cd342e89be6a1d76f7
Pengcheng Tang <tupachydralisk@gmail.com>
2025-02-18 10:46:36 +0800

    Revert "Mix original audio stream of video track with audio track"

--- vmps/video/utils.py
+++ vmps/video/utils.py
@@ -3,7 +3,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
 def get_video_codec(filepath):
     try:
         probe = ffmpeg.probe(filepath)
@@ -13,16 +12,3 @@ def get_video_codec(filepath):
 
     video_stream = next((stream for stream in probe["streams"] if stream["codec_type"] == "video"), None)
     return video_stream["codec_name"] if video_stream else None
-
-
-def has_audio_track(filepath):
-    try:
-        probe = ffmpeg.probe(filepath)
-        streams = probe.get('streams', [])
-        for stream in streams:
-            if stream.get('codec_type') == 'audio':
-                return True
-        return False
-    except ffmpeg.Error as e:
-        logger.error(f"An error occurred when ffprobe {filepath}: {e.stderr.decode('utf-8')}")
-        raise e
